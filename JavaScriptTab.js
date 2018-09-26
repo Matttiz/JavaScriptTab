3e0828786530f74dcce8dea85d7f4832a94995e5
Matttiz <34661703+Matttiz@users.noreply.github.com>
2018-09-26 22:57:11 +0200

    Update JavaScriptTab.js

--- JavaScriptTab.js
+++ JavaScriptTab.js
@@ -1,5 +1,8 @@
-javascript:(function() { function getElementByXpath(path) { return 
-document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, 
-null).singleNodeValue } 
-getElementByXpath("(//*[contains(@class,'lcontrast')])[4]").style. 
-backgroundColor="black"; })(); 
+javascript:(
+  function() { 
+    function getElementByXpath(path) { 
+      return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue 
+    } 
+    getElementByXpath("(//*[contains(@class,'lcontrast')])[4]").style.backgroundColor="black"; 
+  }
+)(); 
